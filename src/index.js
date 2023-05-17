681f30141812e5465ddf1cd3d1f34cb5bdb7174d
orshah <orshpov@gmail.com>
2023-05-17 14:30:42 -0500

    downgrade

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+// react 17.0.2
+
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
