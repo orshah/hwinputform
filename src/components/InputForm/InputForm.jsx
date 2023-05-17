bec70538f8feda97038162b34984a706d9b67627
orshah <orshpov@gmail.com>
2023-05-17 13:36:28 -0500

    upd

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -1,5 +1,4 @@
 import "./inputForm.style.css";
-// import * as React from "react";
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
