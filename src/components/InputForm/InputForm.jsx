7d99e696242fbe4af4bd2119fe6d3da1c937e58d
orshah <orshpov@gmail.com>
2023-05-17 01:18:33 -0500

    almost done

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -1,39 +1,18 @@
 import "./inputForm.style.css";
 import * as React from "react";
 import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
 
 const theme = createTheme();
 
@@ -59,9 +38,8 @@ export default function InputForm() {
             alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
+          <LocalShippingOutlinedIcon fontSize="large" />
+
           <Typography component="h1" variant="h5">
             Delivery Form
           </Typography>
@@ -92,18 +70,13 @@ export default function InputForm() {
               autoComplete="address"
             />
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DatePicker />
+              <DatePicker sx={{ mt: 2 }} />
             </LocalizationProvider>
-
+            <br></br>
             <FormControlLabel
-              control={
-                <Checkbox
-                  value="remember"
-                  color="primary"
-                  className="checkbox"
-                />
-              }
-              label="Signiture"
+              sx={{ mt: 2 }}
+              control={<Checkbox value="remember" color="success" />}
+              label="Signature"
             />
             <TextField
               margin="normal"
@@ -115,29 +88,8 @@ export default function InputForm() {
               id="number"
               autoComplete="phone-number"
             />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
   );
